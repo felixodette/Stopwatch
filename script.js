c39430a0dd35510d6203b6d9391e760e6aa6fcbb
Felix Odete <felix.odette@gmail.com>
2022-05-16 18:09:11 +0300

    gitflow-feature-stash: target-elements

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector('.class').textContent = "I'm the querySelector for class works.";
+document.querySelector('#id_query').textContent = "I'm the querySelector for id works";
+document.getElementById('id').textContent = "The get elementByID works";
